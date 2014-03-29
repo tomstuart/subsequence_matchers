759c6f9f61d3143a0a2644d5da7670c5e69fb72a
Tom Stuart <tom@codon.com>
2014-03-29 10:58:09 +0000

    Talk about collections, not arrays
    This matcher should work on any Enumerable subject.

--- lib/subsequence_matchers.rb
+++ lib/subsequence_matchers.rb
@@ -2,22 +2,22 @@ module SubsequenceMatchers
   extend RSpec::Matchers::DSL
 
   matcher :include_subsequence do |*subsequences|
-    match do |array|
-      perform_match(array, subsequences, :all?)
+    match do |collection|
+      perform_match(collection, subsequences, :all?)
     end
 
-    match_when_negated do |array|
-      perform_match(array, subsequences, :none?)
+    match_when_negated do |collection|
+      perform_match(collection, subsequences, :none?)
     end
 
     private
 
-    def perform_match(array, subsequences, predicate)
-      subsequences.send(predicate) { |subsequence| subsequence?(array, subsequence) }
+    def perform_match(collection, subsequences, predicate)
+      subsequences.send(predicate) { |subsequence| subsequence?(collection, subsequence) }
     end
 
-    def subsequence?(array, subsequence)
-      subsequence.empty? || array.each_cons(subsequence.length).any?(&subsequence.method(:==))
+    def subsequence?(collection, subsequence)
+      subsequence.empty? || collection.each_cons(subsequence.length).any?(&subsequence.method(:==))
     end
   end
 end
--- spec/subsequence_matchers_spec.rb
+++ spec/subsequence_matchers_spec.rb
@@ -3,65 +3,65 @@ require 'subsequence_matchers'
 describe 'include_subsequence matcher' do
   include SubsequenceMatchers
 
-  let(:array) { [1, 2, 3, 4, 5] }
+  let(:collection) { [1, 2, 3, 4, 5] }
 
   it 'matches subsequence at the start' do
-    expect(array).to include_subsequence([1, 2, 3])
+    expect(collection).to include_subsequence([1, 2, 3])
   end
 
   it 'matches subsequence at the end' do
-    expect(array).to include_subsequence([3, 4, 5])
+    expect(collection).to include_subsequence([3, 4, 5])
   end
 
   it 'matches subsequence in the middle' do
-    expect(array).to include_subsequence([2, 3, 4])
+    expect(collection).to include_subsequence([2, 3, 4])
   end
 
   it 'doesn\'t match subsequence in the wrong order' do
-    expect(array).not_to include_subsequence([1, 3, 2])
+    expect(collection).not_to include_subsequence([1, 3, 2])
   end
 
-  it 'matches entire sequence' do
-    expect(array).to include_subsequence([1, 2, 3, 4, 5])
+  it 'matches entire collection' do
+    expect(collection).to include_subsequence([1, 2, 3, 4, 5])
   end
 
   it 'matches single-element subsequence' do
-    expect(array).to include_subsequence([3])
+    expect(collection).to include_subsequence([3])
   end
 
   it 'matches empty subsequence' do
-    expect(array).to include_subsequence([])
+    expect(collection).to include_subsequence([])
   end
 
-  context 'even when the array is empty' do
-    let(:array) { [] }
+  context 'even when the collection is empty' do
+    let(:collection) { [] }
 
     it 'matches empty subsequence' do
-      expect(array).to include_subsequence([])
+      expect(collection).to include_subsequence([])
     end
   end
 
-  it 'doesn\'t match a subsequence that\'s longer than the array' do
-    expect(array).not_to include_subsequence([1, 2, 3, 4, 5, 6])
+  it 'doesn\'t match a subsequence that\'s longer than the collection' do
+    expect(collection).not_to include_subsequence([1, 2, 3, 4, 5, 6])
   end
 
   it 'matches multiple subsequences' do
-    expect(array).to include_subsequence([1, 2, 3], [2, 3, 4], [4, 5])
+    expect(collection).to include_subsequence([1, 2, 3], [2, 3, 4], [4, 5])
   end
 
   it 'doesn\'t match multiple arguments if all are not subsequences' do
-    expect(array).not_to include_subsequence([1, 3, 2], [2, 4, 3], [5, 4])
+    expect(collection).not_to include_subsequence([1, 3, 2], [2, 4, 3], [5, 4])
   end
 
   it 'checks that every argument is a subsequence when not negated' do
     expect {
-      expect(array).to include_subsequence([1, 2, 3], [2, 4, 3], [4, 5])
+      expect(collection).to include_subsequence([1, 2, 3], [2, 4, 3], [4, 5])
     }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 
   it 'checks that every argument is not a subsequence when negated' do
     expect {
-      expect(array).not_to include_subsequence([1, 2, 3], [2, 4, 3], [4, 5])
+      expect(collection).not_to include_subsequence([1, 2, 3], [2, 4, 3], [4, 5])
     }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
   end
 end
